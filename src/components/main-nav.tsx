ae69bbd5e22c4a50823c6917fb7453c22742b983
Akshay Kumar <akashaykumar27687@gmail.com>
2025-09-18 06:50:05 +0000

    now i want the website to be responsive like when i click on dashboard o

--- src/components/main-nav.tsx
+++ src/components/main-nav.tsx
@@ -70,6 +70,7 @@ export default function MainNav() {
           <Link
             key={link.href}
             href={link.href}
+            prefetch={true}
             className={cn(
                 "flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary",
                 pathname === link.href && "bg-muted text-primary"
